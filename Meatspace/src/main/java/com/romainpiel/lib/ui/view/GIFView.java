bbd9054419c28689bbfa4ba4b208102fb511a582
Romain Piel <romain.piel@gmail.com>
2013-11-08 09:53:50 +0000

    GIFView: fixed sizing

--- Meatspace/src/main/java/com/romainpiel/lib/ui/view/GIFView.java
+++ Meatspace/src/main/java/com/romainpiel/lib/ui/view/GIFView.java
@@ -1,13 +1,14 @@
 package com.romainpiel.lib.ui.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.romainpiel.lib.gif.decoder.GIFDecode;
-import com.romainpiel.lib.utils.Debug;
 
 import java.io.ByteArrayInputStream;
 
@@ -15,6 +16,7 @@ public class GIFView extends View {
 
     private Paint paint;
 	private GIFDecode decode;
+    private Rect src, dst;
 
     public GIFView(Context context) {
         super(context);
@@ -33,6 +35,8 @@ public class GIFView extends View {
 
     private void init() {
         paint = new Paint();
+        src = new Rect();
+        dst = new Rect();
     }
 
     public void setImage(byte[] bytes) {
@@ -43,7 +47,32 @@ public class GIFView extends View {
 	@Override
 	protected void onDraw(Canvas canvas) {
         if (decode != null && decode.getFrameCount() > 0) {
-            canvas.drawBitmap(decode.next(), 0, 0, paint);
+
+            Bitmap bmp = decode.next();
+
+            boolean portrait = bmp.getWidth() < bmp.getHeight();
+
+            int bWidth = bmp.getWidth();
+            int bHeight = bmp.getHeight();
+
+            int cWidth = canvas.getWidth();
+            int cHeight = canvas.getHeight();
+
+            int width = portrait? bWidth : bHeight * cWidth / cHeight;
+            int height = portrait? bWidth * cHeight / cWidth : bHeight;
+
+            src.left = bWidth/2 - width/2;
+            src.top = bHeight/2 - height/2;
+            src.right = bWidth/2 + width/2;
+            src.bottom = bHeight/2 + height/2;
+
+            dst.left = 0;
+            dst.top = 0;
+            dst.right = canvas.getWidth();
+            dst.bottom = canvas.getHeight();
+
+            canvas.drawBitmap(bmp, src, dst, paint);
+
             postInvalidateDelayed(150);
         }
 	}
