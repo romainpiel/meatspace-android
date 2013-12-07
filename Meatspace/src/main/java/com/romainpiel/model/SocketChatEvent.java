1edebb9f7b22b1c2a8260a54dc61046a6e6cfa71
Romain Piel <romain.piel@gmail.com>
2013-12-07 16:15:17 +0000

    bug fix #21

---
+++ Meatspace/src/main/java/com/romainpiel/model/SocketChatEvent.java
@@ -0,0 +1,33 @@
+package com.romainpiel.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * MeatspaceProject
+ * User: romainpiel
+ * Date: 07/12/2013
+ * Time: 15:53
+ */
+public class SocketChatEvent {
+
+    private String name;
+    private List<ChatWrapper> args;
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Chat> getChats() {
+        if (args == null) return null;
+        List<Chat> result = new ArrayList<Chat>();
+        for (ChatWrapper chatWrapper : args) {
+            result.add(chatWrapper.chat);
+        }
+        return result;
+    }
+
+    private class ChatWrapper {
+        private Chat chat;
+    }
+}
