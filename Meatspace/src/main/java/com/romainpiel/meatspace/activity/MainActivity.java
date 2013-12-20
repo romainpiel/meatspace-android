e4797119e34a79072f7d9569994b11853dc0fc29
Romain Piel <romain.piel@gmail.com>
2013-12-20 10:53:07 +0000

    corrected buildtypes to fit new gradle plugin version

--- Meatspace/src/main/java/com/romainpiel/meatspace/activity/MainActivity.java
+++ Meatspace/src/main/java/com/romainpiel/meatspace/activity/MainActivity.java
@@ -1,8 +1,6 @@
 package com.romainpiel.meatspace.activity;
 
 import android.app.AlertDialog;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -22,7 +20,6 @@ import com.romainpiel.lib.bus.MuteEvent;
 import com.romainpiel.lib.helper.PreferencesHelper;
 import com.romainpiel.lib.ui.fragment.ChatFragment;
 import com.romainpiel.lib.ui.fragment.SettingsFragment;
-import com.romainpiel.lib.utils.Debug;
 import com.romainpiel.meatspace.BuildConfig;
 import com.romainpiel.meatspace.R;
 import com.romainpiel.meatspace.service.ChatService;
@@ -99,14 +96,9 @@ public class MainActivity extends FragmentActivity {
     private void showAboutDialog() {
         ViewGroup view = (ViewGroup) View.inflate(this, R.layout.dialog_about, null);
 
-        try {
-            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-            TextView versionTv = (TextView) view.findViewById(R.id.dialog_about_version);
-            versionTv.setText(String.format(getString(R.string.dialog_about_version),
-                    pInfo.versionName, pInfo.versionCode));
-        } catch (PackageManager.NameNotFoundException e) {
-            Debug.out(e);
-        }
+        TextView versionTv = (TextView) view.findViewById(R.id.dialog_about_version);
+        versionTv.setText(String.format(getString(R.string.dialog_about_version),
+                BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE));
 
         TextView messageTv = (TextView) view.findViewById(R.id.dialog_about_message);
         messageTv.setText(Html.fromHtml(getString(R.string.dialog_about_content)));
